fb1c557e29e38d034b91e20dd3a42349460600df
federuiz <federicoruiz@sirius.com.ar>
2019-05-15 17:36:42 -0300

    Initial Commit

---
+++ rns-ios/rns_ios.h
@@ -0,0 +1,19 @@
+//
+//  rns_ios.h
+//  rns-ios
+//
+//  Created by Fede Ruiz on 15/05/2019.
+//  Copyright © 2019 Sirius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for rns_ios.
+FOUNDATION_EXPORT double rns_iosVersionNumber;
+
+//! Project version string for rns_ios.
+FOUNDATION_EXPORT const unsigned char rns_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <rns_ios/PublicHeader.h>
+
+
